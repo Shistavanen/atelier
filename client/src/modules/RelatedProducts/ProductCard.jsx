bb3d818e5522f7fdc0eacda2209121bf16a16c4c
Nick Hays <nicholasahays@gmail.com>
2023-03-30 23:54:41 -0700

    Removes unnecessary color style, as this was moved to style sheet

--- client/src/modules/RelatedProducts/ProductCard.jsx
+++ client/src/modules/RelatedProducts/ProductCard.jsx
@@ -8,14 +8,12 @@ export default function ProductCard({ productCategory, productName, productPrice
 
   return (
     <Card>
-      <div style={{ color: '#25383C'}}>
-        <img src={productImage} width='200' height='225' style={{objectFit: 'cover'}} />
-        <div style={{marginLeft: '5px', display:'flex', flexDirection: 'column'}}>
-          <small>{productCategory.toUpperCase()}</small>
-          <strong>{productName}</strong>
-          <small>${productPrice.salePrice ? <div style={{color: 'red'}}>{productPrice.salePrice}</div> : productPrice.originalPrice }</small>
-          <StarRating productRatings={productRatings} starWidth={RELATED_PRODUCTS_STAR_WIDTH} starHeight={RELATED_PRODUCTS_STAR_HEIGHT}/>
-        </div>
+      <img src={productImage} width='200' height='225' style={{objectFit: 'cover'}} />
+      <div style={{marginLeft: '5px', display:'flex', flexDirection: 'column'}}>
+        <small>{productCategory.toUpperCase()}</small>
+        <strong>{productName}</strong>
+        <small>${productPrice.salePrice ? <div style={{color: 'red'}}>{productPrice.salePrice}</div> : productPrice.originalPrice }</small>
+        <StarRating productRatings={productRatings} starWidth={RELATED_PRODUCTS_STAR_WIDTH} starHeight={RELATED_PRODUCTS_STAR_HEIGHT}/>
       </div>
     </Card>
   )

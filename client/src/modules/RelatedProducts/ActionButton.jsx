f4fff750426e1562ed970c8dc04b51d18f2d74c0
Nick Hays <nicholasahays@gmail.com>
2023-03-31 03:03:40 -0700

    Adds onClick and background color props to Action Button

--- client/src/modules/RelatedProducts/ActionButton.jsx
+++ client/src/modules/RelatedProducts/ActionButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function ActionButton({ children }) {
+export default function ActionButton({ clickHandler, bgColor, children }) {
 
   return (
-    <button className='actionButton' >
+    <button className='actionButton' onClick={clickHandler} style={{backgroundColor: bgColor}}>
       {children}
     </button>
   )

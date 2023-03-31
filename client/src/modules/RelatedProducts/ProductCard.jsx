3401dd43f67bbfc326bd9910c3fa15749cbe884c
Nick Hays <nicholasahays@gmail.com>
2023-03-30 23:43:02 -0700

    Creates ProductCard component using Card Wrapper

---
+++ client/src/modules/RelatedProducts/ProductCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './Card.jsx';
+import StarRating from '../../utils/StarRating.jsx';
+
+export default function ProductCard({ productCategory, productName, productPrice, productImage, productRatings }) {
+  const RELATED_PRODUCTS_STAR_WIDTH = 16;
+  const RELATED_PRODUCTS_STAR_HEIGHT = 21;
+
+  return (
+    <Card>
+      <div style={{ color: '#25383C'}}>
+        <img src={productImage} width='200' height='225' style={{objectFit: 'cover'}} />
+        <div style={{marginLeft: '5px', display:'flex', flexDirection: 'column'}}>
+          <small>{productCategory.toUpperCase()}</small>
+          <strong>{productName}</strong>
+          <small>${productPrice.salePrice ? <div style={{color: 'red'}}>{productPrice.salePrice}</div> : productPrice.originalPrice }</small>
+          <StarRating productRatings={productRatings} starWidth={RELATED_PRODUCTS_STAR_WIDTH} starHeight={RELATED_PRODUCTS_STAR_HEIGHT}/>
+        </div>
+      </div>
+    </Card>
+  )
+}
+
+
